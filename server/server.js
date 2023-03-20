406938ce57e22ebaebf081e941ae1418ee24ecc3
Alexander <alexanderblanchard@yahoo.com>
2023-03-20 13:21:08 +0000

    updated cors parameters

--- server/server.js
+++ server/server.js
@@ -5,7 +5,9 @@ const app = express()
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:3000", "https://to-do-app-mern-stack.onrender.com" ]
+}))
 
 
 mongoose.connect('mongodb+srv://todomern:Jacobjames0304@todo.pteecks.mongodb.net/ToDoAppMERN?retryWrites=true&w=majority', {
